b4bbae0ff5087b9335e4be491f9037884deb3ada
pievit <61455960+pievit@users.noreply.github.com>
2023-01-09 15:44:28 +0100

    Add files via upload

---
+++ service-discovery-server/src/main/java/com/example/servicediscoveryserver/ServiceDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicediscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryServerApplication.class, args);
+	}
+
+}
