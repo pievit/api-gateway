51dad619003dee3c60b101d10110af7f377c15bf
pie.vit <pietro.vitale@gmail.com>
2023-02-01 18:26:33 +0100

    rimozione eureka e aggiunta zuul per il routing

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,19 +1,16 @@
 package com.example.apigateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableZuulProxy
 
 public class ApiGatewayApplication {
 
@@ -21,18 +18,12 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
-}
 
-
-@RestController
-class ServiceInstanceRestController {
-
-	@Autowired
-	private DiscoveryClient discoveryClient;
-
-	@RequestMapping("/service-instances/{applicationName}")
-	public List<ServiceInstance> serviceInstancesByApplicationName(
-			@PathVariable String applicationName) {
-		return this.discoveryClient.getInstances(applicationName);
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder)
+	{
+		return builder.routes().route("about_route", r -> r.path("/about").uri("http://example.com")).build();
 	}
 }
+
+
