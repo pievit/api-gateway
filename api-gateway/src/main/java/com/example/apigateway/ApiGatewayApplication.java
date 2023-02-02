d3bd51579b02e909b42208b0f6948f50cdd0094d
pie.vit <pietro.vitale@gmail.com>
2023-02-02 12:12:34 +0100

    rimozione zuul (test)

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,27 +2,13 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
-@EnableZuulProxy
 
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
-
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder)
-	{
-		return builder.routes().route("about_route", r -> r.path("/about").uri("http://example.com")).build();
 	}
 }
 
